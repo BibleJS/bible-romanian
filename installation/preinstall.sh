605866c477aac4420757816475d97ec1f3de7a90
Ionică Bizău <bizauionica@yahoo.com>
2014-07-24 22:14:41 +0300

    Use "O" parameter instead

--- installation/preinstall.sh
+++ installation/preinstall.sh
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/BibleJS/content/38ddf33a4ec62e75378f43834d1d9559a19545d9/verses.ro.json -P all-verses.json
+wget https://raw.githubusercontent.com/BibleJS/content/38ddf33a4ec62e75378f43834d1d9559a19545d9/verses.ro.json -O all-verses.json
