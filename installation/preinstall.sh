a2649eca40bd4b5d1ecc857ed125db3ffe5c930f
Ionică Bizău <bizauionica@yahoo.com>
2014-07-25 09:01:46 +0300

    Download books

--- installation/preinstall.sh
+++ installation/preinstall.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
 ALL_VERSES_PATH="all-verses.json"
+ALL_BOOKS_PATH="all-books.json"
+
 if [ ! -f $ALL_VERSES_PATH ]; then
-    wget http://goo.gl/k7ZySl -O $ALL_VERSES_PATH
+    wget http://goo.gl/w9xWL5 -O $ALL_VERSES_PATH
+fi
+
+if [ ! -f $ALL_BOOKS_PATH ]; then
+    wget http://goo.gl/P71XZQ -O $ALL_BOOKS_PATH
 fi
