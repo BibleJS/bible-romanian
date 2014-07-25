784bf6d29027f33639001a40f00f0ffb68198d65
Ionică Bizău <bizauionica@yahoo.com>
2014-07-25 08:33:16 +0300

    Added getVerse method

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+// Dependencies
+var ReferenceParser = require("bible-reference-parser");
+
 /**
- * findQuery
+ * find
  * This function searches returns the objects from an array
  * that contains objects.
  *
- * @name findQuery
+ * @name find
  * @function
  * @param {Array} array The data array where the documents will be searched.
  * @param {Object} query A Mongo-like filter.
  * @return {Array} Documents that match the query.
  */
-function findQuery (array, query) {
+function find(array, query) {
 
     var col = array
       , res = []
@@ -43,5 +46,44 @@ function findQuery (array, query) {
     return res;
 }
 
-const ALL_VERSES = require ("./all-verses.json");
+// Constants
+const ALL_VERSES = require("./all-verses.json");
 
+var RomanianBible = module.exports = {};
+
+/**
+ * getVerse
+ * Fetches the verses that were requested in reference
+ *
+ *  e.g. Geneza 1:1    - returns one verse
+ *    or Geneza 1:1,2  - returns two verses (1 and 2)
+ *    or Geneza 1:1-10 - returns the verses 1 - 10
+ *    or Geneza 1      - returns the whole chapter
+ *
+ * @name getVerse
+ * @function
+ * @param {String} reference Bible verse reference
+ * @param {Function} callback The callback function
+ * @return
+ */
+RomanianBible.getVerse = function (reference, callback) {
+
+    // Parse reference
+    var parsed = ReferenceParser(reference);
+    if (!parsed) { return callback("Cannot parse the input."); }
+
+    // Build the query
+    var query = {
+        bookname:   parsed.book
+      , chapter:    parsed.chapter
+      , verse:      parsed.verses
+    };
+
+    // "ALL" is special
+    if (query.verse === "ALL") {
+        delete query.verse;
+    }
+
+    // Send the response
+    callback (null, find(ALL_VERSES, query));
+};
